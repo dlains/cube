58d7e2d6ad80204c761a23e3aab966da67bfbea9
David Lains <dlains@gmail.com>
2018-06-21 17:34:02 -0700

    Some more scanner commenting.

--- src/scanner.c
+++ src/scanner.c
@@ -1,16 +1,42 @@
+/** @file scanner.c
+ *
+ * @brief Implementation of the scanner module.
+ *
+ * @author David J. Lains (dlains)
+ * @bug No known bugs
+ */
+
 #include "common.h"
 #include "keywords.h"
 #include "scanner.h"
 #include "memory.h"
 
+/** @brief The Scanner struct holds the current scanning information.
+ */
 typedef struct
 {
-  const char *start;
-  const char *current;
-  int line;
+  const char *start;     /**< Marks the start of the current token. */
+  const char *current;   /**< Markes the current position of the scan. */
+  int line;              /**< Tracks the current line number for error reporting. */
 } Scanner;
 
 Scanner scanner;
+
+// Private scanner methods.
+static Token makeToken(TokenType type);
+static Token errorToken(const char *message);
+static Token string();
+static Token number();
+static Token identifier();
+static TokenType identifierType();
+static char advance();
+static char peek();
+static char peekNext();
+static void skipWhitespace();
+static bool isAtEnd();
+static bool match(char expected);
+static bool isAlpha(char c);
+static bool isDigit(char c);
 
 void initScanner(const char *source)
 {
@@ -86,22 +112,38 @@ Token nextToken()
   return errorToken("Unexpected character.");
 }
 
-static bool isAtEnd()
-{
-  return *scanner.current == '\0';
-}
-
+/** @brief Make a new token with the given token type.
+ *
+ * Creates a new token with the given token type and fills in the
+ * other information. If the token lexeme is longer than LEXEME_LEN
+ * an error token will be created instead.
+ *
+ * @param type The token type to create.
+ * @return Token the newly created token.
+ */
 static Token makeToken(TokenType type)
 {
+  int length = scanner.current - scanner.start;
+  if(length > LEXEME_LEN - 1)
+    return errorToken("Identifier length is too long.");
+
   Token token;
   token.type = type;
-  memcpy(token.lexeme, scanner.start, scanner.current - scanner.start);
-  token.lexeme[scanner.current - scanner.start] = '\0';
+  memcpy(token.lexeme, scanner.start, length);
+  token.lexeme[length] = '\0';
   token.line = scanner.line;
 
   return token;
 }
 
+/** @brief Make an error token to signal a scan error.
+ *
+ * Creates a new token with a token type of TOKEN_ERROR. The
+ * message passed in will be copied to the lexeme field.
+ *
+ * @param message The error message for this token error.
+ * @return Token The newly created error token.
+ */
 static Token errorToken(const char *message)
 {
   Token token;
@@ -112,62 +154,21 @@ static Token errorToken(const char *message)
   return token;
 }
 
-static char advance()
-{
-  scanner.current++;
-  return scanner.current[-1];
-}
-
-static bool match(char expected)
-{
-  if(isAtEnd())
-    return false;
-
-  if(*scanner.current != expected)
-    return false;
-
-  scanner.current++;
-  return true;
-}
-
-static char peek()
-{
-  return *scanner.current;
-}
-
-static char peekNext()
-{
-  if(isAtEnd())
-    return '\0';
-  return scanner.current[1];
-}
-
-static void skipWhitespace()
-{
-  for(;;)
-  {
-    char c = peek();
-    switch(c)
-    {
-      case ' ':
-      case '\r':
-      case '\t':
-        advance();
-        break;
-      case '\n':
-        scanner.line++;
-        advance();
-        break;
-      case '#':
-        while(peek() != '\n' && !isAtEnd())
-          advance();
-        break;
-      default:
-        return;
-    }
-  }
-}
-
+/** @brief Scan for a string literal.
+ *
+ * The string function is called when a double quote character
+ * if found in the source. It scans forward until if finds
+ * either another double quote character or hits the end of
+ * the source code.
+ *
+ * If it finds the ending double quote it creates a new string
+ * token.
+ *
+ * If it hits the end of the file without finding the closing
+ * double quote an error token is returned.
+ *
+ * @return Token Either the string token or an error token.
+ */
 static Token string()
 {
   while(peek() != '"' && !isAtEnd())
@@ -222,6 +223,67 @@ static TokenType identifierType()
     return type;
 
   return TOKEN_IDENTIFIER;
+}
+
+static char advance()
+{
+  scanner.current++;
+  return scanner.current[-1];
+}
+
+static char peek()
+{
+  return *scanner.current;
+}
+
+static char peekNext()
+{
+  if(isAtEnd())
+    return '\0';
+  return scanner.current[1];
+}
+
+static void skipWhitespace()
+{
+  for(;;)
+  {
+    char c = peek();
+    switch(c)
+    {
+      case ' ':
+      case '\r':
+      case '\t':
+        advance();
+        break;
+      case '\n':
+        scanner.line++;
+        advance();
+        break;
+      case '#':
+        while(peek() != '\n' && !isAtEnd())
+          advance();
+        break;
+      default:
+        return;
+    }
+  }
+}
+
+static bool isAtEnd()
+{
+  return *scanner.current == '\0';
+}
+
+static bool match(char expected)
+{
+  if(isAtEnd())
+    return false;
+
+  if(*scanner.current != expected)
+    return false;
+
+  scanner.current++;
+  return true;
 }
 
 static bool isAlpha(char c)
--- src/scanner.h
+++ src/scanner.h
@@ -44,20 +44,4 @@ void initScanner(const char *source);
  */
 Token nextToken();
 
-// Private scanner methods.
-static Token makeToken(TokenType type);
-static Token errorToken(const char *message);
-static bool isAtEnd();
-static char advance();
-static bool match(char expected);
-static char peek();
-static char peekNext();
-static void skipWhitespace();
-static Token string();
-static Token number();
-static Token identifier();
-static bool isAlpha(char c);
-static bool isDigit(char c);
-static TokenType identifierType();
-
 #endif // SCANNER_H
