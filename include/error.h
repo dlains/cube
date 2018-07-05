b20833a18d1a897a7f9e4015eb06d60adef33ca6
David Lains <dlains@gmail.com>
2018-07-04 18:31:00 -0700

    Added documentation for the error module.

--- include/error.h
+++ include/error.h
@@ -1,6 +1,28 @@
+/** @file error.h
+ *
+ * @brief Define error handling and reporting functions.
+ *
+ * This module will house the error handling and reporting
+ * functions that will be used by Cube. Currently only a
+ * single function is defined, but over time other functions
+ * will be added.
+ *
+ * @author David J. Lains (dlains)
+ * @bug No known bugs.
+ */
+
 #ifndef ERROR_H
 #define ERROR_H
 
+/** @brief Stop all processing and print an error message.
+ *
+ * Output an error message along with the source file and
+ * line number that generated the error.
+ *
+ * @param message An error message to be printed to stderr.
+ * @param file The source file name where the error occurred.
+ * @param line The source line number where the error occurred.
+ */
 void die(const char *message, const char *file, int line);
 
 #endif // ERROR_H
--- src/error.c
+++ src/error.c
@@ -1,7 +1,25 @@
+/** @file error.c
+ *
+ * @brief The implementation of the error handling functions.
+ *
+ * @author David J. Lains (dlains)
+ * @bug No known bugs.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "error.h"
 
+/** @brief Display an error message and stop processing.
+ *
+ * Used in cases where recovery is not on option, typically
+ * when checking for NULL values returned from memory allocation
+ * routines.
+ *
+ * @param message The error message to display.
+ * @param file The source file name where the error occurred.
+ * @param line The source file line where the error occurred.
+ */
 void die(const char *message, const char *file, int line)
 {
   fprintf(stderr, "Cube error");
