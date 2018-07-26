938e87add7c5ff306cfc729df5c2ff650e6d2ed4
David Lains <dlains@gmail.com>
2018-07-26 11:45:07 -0700

    Moved error handling code from the compiler to the error module.

--- include/error.h
+++ include/error.h
@@ -10,9 +10,10 @@
  * @author David J. Lains (dlains)
  * @bug No known bugs.
  */
-
 #ifndef ERROR_H
 #define ERROR_H
+
+#include "token.h"
 
 /** @brief Stop all processing and print an error message.
  *
@@ -24,5 +25,15 @@
  * @param line The source line number where the error occurred.
  */
 void die(const char *message, const char *file, int line);
+
+/** @brief Display a parse error.
+ *
+ * Prints an error message to stderr regarding the given token
+ * and the given message.
+ *
+ * @param token The Token that generated the error.
+ * @param message A message with details about the error.
+ */
+void parse_error(Token *token, const char *message);
 
 #endif // ERROR_H
--- src/error.c
+++ src/error.c
@@ -33,3 +33,33 @@ void die(const char *message, const char *file, int line)
   fflush(stderr);
   abort();
 }
+
+/** @brief Display a parse error.
+ *
+ * Prints an error message to stderr regarding the given token
+ * and the given message.
+ *
+ * @param token The Token that generated the error.
+ * @param message A message with details about the error.
+ */
+void parse_error(Token *token, const char *message)
+{
+  fprintf(stderr, "[%d:%d] Error", token->line, token->col);
+
+  if(token->type == TOKEN_EOF)
+  {
+    fprintf(stderr, " at EOF");
+  }
+  else if(token->type == TOKEN_ERROR)
+  {
+    // Nothing
+  }
+  else
+  {
+    fprintf(stderr, " at '%s'", token->lexeme);
+  }
+
+  fprintf(stderr, ": %s\n", message);
+}
+
+
