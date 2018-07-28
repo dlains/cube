c5ff0a16d45db5f817a7afe5d15aa5b2d041b1e1
David Lains <dlains@gmail.com>
2018-07-28 10:17:09 -0700

    Added preliminary support for Modulo operations.

--- src/debug.c
+++ src/debug.c
@@ -42,6 +42,8 @@ int disassembleInstruction(Chunk *chunk, int offset)
       return simpleInstruction("OP_MULTIPLY", offset);
     case OP_DIVIDE:
       return simpleInstruction("OP_DIVIDE", offset);
+    case OP_MODULUS:
+      return simpleInstruction("OP_MODULUS", offset);
     case OP_POWER:
       return simpleInstruction("OP_POWER", offset);
     case OP_NEGATE:
